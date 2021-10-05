c3b948e148074ad5d13162510a3ec58c9e828e0f
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-10-05 15:48:04 +0530

    Looping through an array in JSX to display NewsItems from state

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img className="card-img-top" src={imageUrl} alt="Card image cap" />
+                    <img className="card-img-top" src={imageUrl} alt="Card cap" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-primary">News Details</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">News Details</a>
                     </div>
                 </div>
             </div>
