8e5f0420320f28d7ab84c5b022fdb9d2445f7038
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-10-09 17:18:37 +0530

    Adding categories and propTypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,11 +15,16 @@ export class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/">Home </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/about">About</a>
-              </li>
+              <li className="nav-item"><a className="nav-link" href="/about">About</a> </li>
+              <li className="nav-item"><a className="nav-link" href="/about">business</a> </li>
+              <li className="nav-item"><a className="nav-link" href="/about">entertainment</a></li>
+              <li className="nav-item"><a className="nav-link" href="/about">general</a></li>
+              <li className="nav-item"><a className="nav-link" href="/about">health</a></li>
+              <li className="nav-item"><a className="nav-link" href="/about">science</a></li>
+              <li className="nav-item"><a className="nav-link" href="/about">sports</a></li>
+              <li className="nav-item"><a className="nav-link" href="/about">technology</a></li>
             </ul>
           </div>
         </nav>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
         let {title,description,imageUrl,newsUrl} = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card">
                     <img className="card-img-top" src={!imageUrl?"https://images.livemint.com/img/2021/10/05/600x338/telecom-kBSC--621x414@LiveMint_1633425521178.JPG":imageUrl} alt="Card cap" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
