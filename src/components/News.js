ec9d494a3acb559b57b8a72ddf15552b911e67d6
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-10-10 10:48:01 +0530

    added time, author and news source to News React App

--- src/components/News.js
+++ src/components/News.js
@@ -7,12 +7,12 @@ export class News extends Component {
     static defaultProps = {
         country: 'in',
         pageSize: 8,
-        category : 'general',
+        category: 'general',
     }
 
     static propTypes = {
-        country : PropTypes.string,
-        pageSize : PropTypes.number,
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
         category: PropTypes.string,
     }
     constructor() {
@@ -34,7 +34,7 @@ export class News extends Component {
     handleNextClick = async () => {
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
             console.log("next");
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=014475f3bfe84028bce67eebe6fbe653&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=014475f3bfe84028bce67eebe6fbe653&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true });
             let data = await fetch(url);
             let parsedData = await data.json()
@@ -46,7 +46,7 @@ export class News extends Component {
         }
     }
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=014475f3bfe84028bce67eebe6fbe653&page =${this.state.page-1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=014475f3bfe84028bce67eebe6fbe653&page =${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json()
@@ -60,11 +60,12 @@ export class News extends Component {
         return (
             <div className="container my-3">
                 <h1 className="text-center">NewsMonkey - Top Headlines</h1>
-                {this.state.loading && <Spinner/>}
+                {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""}
+                                imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })}
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title,description,imageUrl,newsUrl} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card">
-                    <img className="card-img-top" src={!imageUrl?"https://images.livemint.com/img/2021/10/05/600x338/telecom-kBSC--621x414@LiveMint_1633425521178.JPG":imageUrl} alt="Card cap" />
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source}
+                    </span>
+                    <img className="card-img-top" src={!imageUrl ? "https://images.livemint.com/img/2021/10/05/600x338/telecom-kBSC--621x414@LiveMint_1633425521178.JPG" : imageUrl} alt="Card cap" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
+                        <h5 className="card-title">{title} </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark">News Details</a>
                     </div>
                 </div>
